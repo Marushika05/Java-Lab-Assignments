7d4e35b3452f951414d29d19b7aa35eb39e65971
Marushika Gupta <marushikagupta@gmail.com>
2026-08-11 09:25:21 +0530

    Update Ques1.java

--- Assignment2/Ques1.java
+++ Assignment2/Ques1.java
@@ -68,4 +68,104 @@ public static void display_total_order(){
 }
 
 //creatinginterface
+interface Discountable{
+    double applydisc();
+}
 
+//regular order
+class RegularOrder extends FoodOrder implements Discountable{
+    public RegularOrder(int orderid, String customername, double amount){
+        super(orderid,customername,amount);
+    }
+
+    //d3livery charge =80/-
+
+    @Override
+    public double calc_deli_charge(){
+        return 80;
+    }
+
+    //discount 10 percent
+    @Override
+    public double applydisc(){
+        return getamount()*0.10;
+    }
+}
+
+//Premium order
+class PremiumOrder extends FoodOrder implements Discountable{
+    public PremiumOrder(int orderid, String customername, double amount){
+        super(orderid,customername,amount);
+    }
+
+    //d3livery charge =80/-
+
+    @Override
+    public double calc_deli_charge(){
+        return 50;
+    }
+
+    //discount 10 percent
+    @Override
+    public double applydisc(){
+        return getamount()*0.15;
+    }
+}
+
+//utility class
+class OrderUtility{
+    //vlaidate amount
+    public static boolean validate_amt(double amount){
+        return amount>0;
+    }
+
+    //val customername
+    public static boolean validate_customer_name(String name){
+        return name!=null && !name.trim().isEmpty();
+    }
+
+    //generate order summary 
+    public static void gen_ord_summary(FoodOrder order)
+    {
+        double discount=((Discountable)order).applydisc();
+        double delicharge=order.calc_deli_charge();
+        double finalamount=order.getamount()-discount+delicharge;
+ //sysytem p[rint commands
+ 
+    }
+}
+
+//driver class
+public class Main{
+    public static void main (String[] args){
+        //cretae an array of food orders
+
+        FoodOrder[] orders =new FoodOrder[6];
+
+        //creating new orders
+        // order[0]=
+
+        ///print cmd
+        
+
+    //display bill for every roder
+    for(FoodOrder order :orders){
+        if(!OrderUtility.validate_customer_name(order.getcustomername())){
+            System.err.println("Invalid Custoemr name");
+            continue;
+        }
+
+        if(!OrderUtility.validate_amt(order.getamount())){
+            System.err.println("Invalid Amount");
+            continue;
+        }
+
+        //generate bil
+        OrderUtility.gen_ord_summary(order);
+    }
+
+    //display total nu,bers of orders
+    System.out.println();
+    FoodOrder.display_total_order();
+    }
+}
